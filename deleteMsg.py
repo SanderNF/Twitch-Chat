9c84179c040cd62fb79a5e9c49e2bc2eebc17036
SanderNF <sandernf@outlook.com>
2025-10-20 18:02:26 +0200

    Add delete messages by user functionality
    Introduced a new function deleteByUser in deleteMsg.py to allow deletion of all messages by a specific user. Updated main.py to handle the CHAT_CLEARED event and trigger user message deletion. Modified index.html to improve the display of deleted messages.

--- deleteMsg.py
+++ deleteMsg.py
@@ -20,4 +20,23 @@ def deleteById(id):
             json.dump([{"msg":f'<p> Delete message failed with error: {e} reseting chat </p>',"id":"error"}], f, ensure_ascii=False, indent=4)
 
 
+def deleteByUser(username):
+    with open('Chat.json', 'r',  encoding='utf-8') as f:
+        ChatJson = json.load(f)
+    for i in range(len(ChatJson)):
+        print(f'i: {ChatJson[i]["id"]}')
+        if (f'{username}</code>' in ChatJson[i]["msg"]):
+            print(f'found msg with id: {i}')
+            ChatJson[i]["msg"] = "deleted"
+            ChatJson[i]["id"] += "-deleted"
+
+    try:
+        with open('Chat.json', 'w',  encoding='utf-8') as f:
+            json.dump(ChatJson, f, ensure_ascii=False, indent=4)
+    except Exception as e:
+        print(f'Delete message failed with error: {e} reseting chat')
+        with open('Chat.json', 'w', encoding='utf-8') as f:
+            json.dump([{"msg":f'<p> Delete message failed with error: {e} reseting chat </p>',"id":"error"}], f, ensure_ascii=False, indent=4)
+
+
 deleteById('none')
