4d26d502f0c72be77fa99e2b44a3b30509baf88f
SanderNF <sandernf@outlook.com>
2026-02-02 18:08:08 +0100

    minor string change

--- keywordDetection.py
+++ keywordDetection.py
@@ -60,9 +60,9 @@ async def icon(msg):
         msgIcon += '<img class="msgIcon" src="SVG/Fish.svg">'
     elif re.search(' fisk*', message):
         msgIcon += '<img class="msgIcon" src="SVG/Fish.svg">'
-    if re.search(':3', msg.text.lower()):
+    if re.search(' :3 ', " "+msg.text.lower()+" "):
         msgIcon += '<img class="msgIcon Colon3" src="SVG/Colon3_v2.svg">'
-    if re.search('4', msg.text.lower()):
+    if re.search(' 4 ', " "+msg.text.lower()+" "):
         msgIcon += '<img class="msgIcon Four" src="SVG/Four.svg">'
     if re.search('four', msg.text.lower()):
         msgIcon += '<img class="msgIcon Four" src="SVG/Four.svg">'
